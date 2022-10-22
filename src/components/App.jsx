4dad4a03d2b43e525718d7d130a234bb1f3d03bd
Tetiana Kotliarova <100161722+Tetiana0114@users.noreply.github.com>
2022-10-22 17:14:40 +0300

    Done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,10 +8,10 @@ import SearchField from 'components/SearchField';
 export class App extends Component {
   state = {
     contacts: [
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    // {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    // {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    // {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    // {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
     ],
     filter: '',
   }
@@ -40,6 +40,20 @@ onFilteredNames = () => {
 onDelete = id => {
     this.setState(prev => ({ contacts: prev.contacts.filter(contact => contact.id !== id) }));
   };
+
+componentDidMount() {
+  const contacts = localStorage.getItem('contacts');
+  const parsedContacts = JSON.parse(contacts);
+  if(parsedContacts) {
+     this.setState({contacts: parsedContacts});
+  }
+}
+
+componentDidUpdate(prevProps, prevState) {
+  if (this.state.contacts !== prevState.contacts) {
+    localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  }
+}
 
 render () {
   const filteredName = this.onFilteredNames();
